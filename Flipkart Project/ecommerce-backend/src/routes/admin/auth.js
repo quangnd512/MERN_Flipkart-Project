f99625e64b53cfae3999b622f919540e4ab3d4a5
quangnd512 <quangnd512@gmail.com>
2023-11-04 10:04:22 +0700

    validation api

--- Flipkart Project/ecommerce-backend/src/routes/admin/auth.js
+++ Flipkart Project/ecommerce-backend/src/routes/admin/auth.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 // Chuyển dữ liệu đã được xử lý từ controller
 const { signup, signin, requireSignin } = require('../../controller/admin/auth');
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../../validators/auth')
 
 // Khởi tạo routes
 const router = express.Router();
 
-router.post('/admin/signin', signin);
-router.post('/admin/signup', signup);
+router.post('/admin/signin', validateSigninRequest, isRequestValidated, signin);
+router.post('/admin/signup', validateSignupRequest, isRequestValidated, signup);
 
 module.exports = router; 
--- Flipkart Project/ecommerce-backend/src/routes/auth.js
+++ Flipkart Project/ecommerce-backend/src/routes/auth.js
@@ -3,13 +3,15 @@ const express = require('express');
 // Chuyển dữ liệu đã được xử lý từ controller
 const { signup, signin, requireSignin } = require('../controller/auth');
 
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../validators/auth')
+
 // Khởi tạo routes
 const router = express.Router();
 
-router.post('/signin', signin);
+router.post('/signin', validateSigninRequest, isRequestValidated, signin);
 
 // Chuyển hướng khi truy cập vào: ../api/signup
-router.post('/signup', signup);
+router.post('/signup', validateSignupRequest, isRequestValidated, signup);
 
 // router.post('/profile',requireSignin, (req, res) => {
 //     res.status(200).json({ user: 'profile' })
