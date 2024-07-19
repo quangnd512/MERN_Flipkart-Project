bf853d2bfe8c8f2c063acba53b79a2cb89477f33
quangnd512 <quangnd512@gmail.com>
2024-07-19 09:52:14 +0700

    add code

--- Flipkart Project/ecommerce-backend/src/routes/product.js
+++ Flipkart Project/ecommerce-backend/src/routes/product.js
@@ -7,9 +7,12 @@ const shortid = require('shortid');
 const path = require('path');
 
 const storage = multer.diskStorage({
+    //Xác định thư mục tải lên
     destination: function (req, file, cb) {
       cb(null, path.join(path.dirname(__dirname), 'uploads'))
     },
+
+    //Xác định tên mới sau khi tải
     filename: function (req, file, cb) {
       cb(null, shortid.generate() + '-' + file.originalname)
     }
@@ -17,6 +20,7 @@ const storage = multer.diskStorage({
 
   const upload = multer({ storage });
 
+// upload.array('productPicture') sẽ upload ảnh vào trường productPicture trong database
 router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture'), createProduct);
 
 module.exports = router;
